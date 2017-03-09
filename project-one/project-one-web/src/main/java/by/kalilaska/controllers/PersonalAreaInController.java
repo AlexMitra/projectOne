e78ce898423912122590f308066fa0e485c74329
Mitra <lovcov.alex@gmail.com>
2017-03-09 14:46:12 +0200

    replaced log4J with properties to logback

--- project-one/project-one-web/src/main/java/by/kalilaska/controllers/PersonalAreaInController.java
+++ project-one/project-one-web/src/main/java/by/kalilaska/controllers/PersonalAreaInController.java
@@ -1,6 +1,5 @@
 package by.kalilaska.controllers;
 
-import org.apache.log4j.Logger;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,7 +12,8 @@ import by.kalilaska.beans.AccountDetailsPageBean;
 @Controller
 public class PersonalAreaInController {
 
-	private final static Logger logger = Logger.getLogger(PersonalAreaInController.class);
+	// private final static Logger logger =
+	// Logger.getLogger(PersonalAreaInController.class);
 
 	@RequestMapping(value = { "/personalArea/userPage.html" }, method = RequestMethod.GET)
 	public ModelAndView loginSuccess() {
@@ -29,7 +29,7 @@ public class PersonalAreaInController {
 	public ModelAndView showPersonalAreaPage(@PathVariable String accountLogin) {
 
 		AccountDetailsPageBean account = getAccountPageBean();
-		logger.info(accountLogin + " log in successfull");
+		// logger.info(accountLogin + " log in successfull");
 		// logger.error(" error message ");
 		// logger.debug(" debug message ");
 		// logger.fatal(" fatal message ");
